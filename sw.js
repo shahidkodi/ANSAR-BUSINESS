91ab7eb7b14ea9d83922ee5abc783161225935d1
shahidkodi <99824232+shahidkodi@users.noreply.github.com>
2026-09-22 21:06:06 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ansar-business-v12';
+const CACHE = 'ansar-business-v15';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
